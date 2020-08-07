a9f1caacf1bd6dc63d8a1ded24b998255fa8ed5f
Rachel Hu <rachelhu09@gmail.com>
2020-08-06 21:54:01 -0700

    8/6

---
+++ Assets/EndTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameObject panel;
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("LEVEL COMPLETE");
+        panel.SetActive(true);
+    }
+}
